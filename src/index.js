67bbf2d240f1f8051147ca5af250981a798775bd
Mudssir <mudssirali98@gmail.com>
2022-08-20 12:29:03 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { ContextProvider } from './Context/context'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'))
+
+ReactDOM.render(
     <React.StrictMode>
-        <ContextProvider>
-            <App />
-        </ContextProvider>
-    </React.StrictMode>
-)
+     <ContextProvider>
+      <App />
+      </ContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
